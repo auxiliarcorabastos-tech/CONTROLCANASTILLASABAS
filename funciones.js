f0a8fa17a418eb008e1fc65802a7b9cef61d98f5
auxiliarcorabastos-tech <auxiliarcorabastos@bancodealimentos.org.co>
2026-09-10 09:55:49 -0500

    Fix measurementId syntax error in funciones.js

--- funciones.js
+++ funciones.js
@@ -35,7 +35,7 @@ const firebaseConfig = {
   storageBucket: "control-ingresos-y-canastillas.firebasestorage.app",
   messagingSenderId: "372736670308",
   appId: "1:372736670308:web:14c2e2614c14ff3dc2bd71",
-  measurementId: "G-N3YMQ2JKZM
+  measurementId: "G-N3YMQ2JKZM"
 };
 firebase.initializeApp(firebaseConfig);
 db = firebase.firestore();
